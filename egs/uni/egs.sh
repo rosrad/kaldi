de9fe98f2500d21355602b3a64dd21ee30d70c34
renbo <justnow.ren@gmail>
2016-09-02 09:53:06 +0800

    [U] update egs.sh

--- egs/uni/egs.sh
+++ egs/uni/egs.sh
@@ -6,13 +6,12 @@ export KALDI_ROOT="/work/local/renbo/kaldi/master/"
 export CORPUS_ROOT="/home/renbo/work/corpus/uni_doa/"
 
 
-tag="raw-gcc"
+tag="cmn_gcc"
 
-for corpus in clean.RIR0.13.vad reverb/t60_0.3
+for corpus in reverb/t60_0.3
 do
     echo "training [$corpus] ..."
     ./init_dnn.py -t $tag $corpus 2>/dev/null 
-
     echo "evaluating ..."
-    ./doa.py  "exp/data_${tag/-/_}_simu_${corpus/-/_}/"  $tag
+    ./doa.py  "exp/data_${tag}_simu_${corpus/\//_}/"  $tag
 done
