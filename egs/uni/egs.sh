01c4f11dbfeb5bf8e64a72f8650550c7bf7b631c
renbo <justnow.ren@gmail>
2016-09-02 18:24:49 +0800

    [U] update egs.sh

--- egs/uni/egs.sh
+++ egs/uni/egs.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 . path.sh
-export KALDI_ROOT="/work/local/renbo/kaldi/master/"
 export CORPUS_ROOT="/home/renbo/work/corpus/uni_doa/"
 
-
 tag="cmn_gcc"
-
-for corpus in reverb/t60_0.3
+#
+# 
+for corpus in clean.RIR0.13.vad reverb/t60_0.3
 do
     echo "training [$corpus] ..."
     ./init_dnn.py -t $tag $corpus 2>/dev/null 
